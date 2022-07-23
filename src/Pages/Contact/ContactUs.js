eb32e3fed6140f9650ecfb8123836c64a50af9cf
said7388 <mdabusaidriyaz@gmail.com>
2022-07-23 20:44:22 +0600

    project ready

--- src/Pages/Contact/ContactUs.js
+++ src/Pages/Contact/ContactUs.js
@@ -4,7 +4,7 @@ import "./Contact.css";
 
 
 function Contact() {
-    
+
     return (
         <div className='mt-md-5 my-3'>
             <p className='contact-sub mt-3 text-center'>Get in touch</p>
@@ -22,10 +22,11 @@ function Contact() {
                 </div>
 
                 <div>
+
                     <iframe
                         title='Meddical'
                         className='medi-map'
-                        src='https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d12099.578392794248!2d-73.99760377578289!3d40.698318937624514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sA108%20Adam%20Street%2C%20New%20York%2C%20NY%20535022!5e0!3m2!1sbn!2sbd!4v1634729385323!5m2!1sbn!2sbd'
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7300.888371851632!2d90.3636722!3d23.802799!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c12cd2f41ad5%3A0xd4eb5975120eaff0!2sMirpur%2010%20Bus%20Stand!5e0!3m2!1sen!2sbd!4v1658586728053!5m2!1sen!2sbd"
                         allowfullscreen=''
                         loading='lazy'></iframe>
                 </div>
